5599fe15302da249e2f214706def4b3939d5db48
Marlon Braga <rdg.brg@gmail.com>
2020-05-04 02:49:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,10 @@
-def collect_names(data) 
-  collectNames = []
 
-  data.map do |dataKey, dataValue|
+def nyc_pigeon_organizer(pigeon_data)
+  collectNames = []
+  infoCollection = []
+  pigeonData = {}
+
+  pigeon_data.map do |dataKey, dataValue|
     dataValue.each do |key, valueNames|
       valueNames.each do |name|
         if !collectNames.include?(name)
@@ -10,17 +13,11 @@ def collect_names(data)
       end
     end 
   end
-  return collectNames
-end
 
-# COLLECT INFO ON PIGEONS 
-def collect_each_info(data, allNames)
-  infoCollection = []
-
-  data.map do |dataKey, dataValue|
+  pigeon_data.map do |dataKey, dataValue|
     dataValue.each do |info, nameValue|
       nameValue.each do |whichName|
-        allNames.each do |name|
+        collectNames.each do |name|
           if whichName == name
             infoCollection << [name, dataKey, info]
           end
@@ -28,13 +25,8 @@ def collect_each_info(data, allNames)
       end
     end
   end
-  return infoCollection
-end
 
-def hash_init(data)
-  pigeonData = {}
-
-  data.map do |dataValues|    
+  infoCollection.map do |dataValues|    
     if !pigeonData[dataValues[0]]
       pigeonData[dataValues[0]] = {}
     end
@@ -45,14 +37,6 @@ def hash_init(data)
       pigeonData[dataValues[0]][dataValues[1]] << dataValues[2].to_s
     end
   end
-  return pigeonData
 
-end
-
-def organize_pigeon_data(data)
-  names = collect_names(data)
-  # p names
-  allDetails = collect_each_info(data, names)
-  p allDetails
-  return hash_init(allDetails)
+  return pigeon_data
 end
