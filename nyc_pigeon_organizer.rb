c8fc714cba0245172303e2390d6eef7514b9f3e1
Marlon Braga <rdg.brg@gmail.com>
2020-05-04 02:45:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,58 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def collect_names(data) 
+  collectNames = []
+
+  data.map do |dataKey, dataValue|
+    dataValue.each do |key, valueNames|
+      valueNames.each do |name|
+        if !collectNames.include?(name)
+          collectNames << name
+        end
+      end
+    end 
+  end
+  return collectNames
+end
+
+# COLLECT INFO ON PIGEONS 
+def collect_each_info(data, allNames)
+  infoCollection = []
+
+  data.map do |dataKey, dataValue|
+    dataValue.each do |info, nameValue|
+      nameValue.each do |whichName|
+        allNames.each do |name|
+          if whichName == name
+            infoCollection << [name, dataKey, info]
+          end
+        end
+      end
+    end
+  end
+  return infoCollection
+end
+
+def hash_init(data)
+  pigeonData = {}
+
+  data.map do |dataValues|    
+    if !pigeonData[dataValues[0]]
+      pigeonData[dataValues[0]] = {}
+    end
+    if !pigeonData[dataValues[0]][dataValues[1]]
+      pigeonData[dataValues[0]][dataValues[1]] = []
+    end
+    if !pigeonData[dataValues[0]][dataValues[1]].include?(dataValues[2])
+      pigeonData[dataValues[0]][dataValues[1]] << dataValues[2].to_s
+    end
+  end
+  return pigeonData
+
+end
+
+def organize_pigeon_data(data)
+  names = collect_names(data)
+  # p names
+  allDetails = collect_each_info(data, names)
+  p allDetails
+  return hash_init(allDetails)
 end
